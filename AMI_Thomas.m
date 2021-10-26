8943167f81a2ae8ed3c2119a3dfa8b0d06fcd9de
LouisNodskov <lnodskov@unomaha.edu>
2021-10-26 18:10:46 -0500

    AMI Thomas speedups

--- AMI_Thomas.m
+++ AMI_Thomas.m
@@ -68,48 +68,48 @@ function [tau,AMI]=AMI_Thomas(x,L)
 % LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
 % NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
 % SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-%% Begin code
+    %% Begin code
 
-% check size of input x
-[m,n]=size(x);
-if m==1
-    x=x';
-elseif m>1 && n>1
-    error('input time series is not a one dimensional vector')
-end
-
-% calculate AMI at each lag
-AMI=zeros(L,2);
-% fprintf('AMI: 00%%')
-for i=1:L
-    AMI(i,1)=i;
-    X=x(1:end-i);
-    Y=x(i+1:end);
-    AMI(i,2)=average_mutual_information([X,Y]);
-    
-%     fprintf(repmat('\b',1,4));
-%     msg=sprintf('%3.0d',floor(i/L*100));
-%     fprintf([msg '%%']);
-end
-
-tau=[];
-for i=2:length(AMI)-1
-    if (AMI(i-1,2)>=AMI(i,2))&&(AMI(i,2)<=AMI(i+1,2))
-        tau(end+1,:)=AMI(i,:);
+    % check size of input x
+    [m,n]=size(x);
+    if m==1
+        x=x';
+    elseif m>1 && n>1
+        error('input time series is not a one dimensional vector')
     end
-end
-ind=find(AMI(:,2)<=(0.2*AMI(1,2)),1,'first'); % finds lag at 20% initial AMI
-if ~isempty(ind)
-    tau(end+1,:)=AMI(ind,:);
-end
-if isempty(tau)
-    if L*1.5>length(x/3)
-        tau=9999;
-    else
-        fprintf('max lag needed to be increased for AMI_Thomas\n')
-        [tau,AMI]=AMI_Thomas20150901(x,floor(L*1.5));
+
+    % calculate AMI at each lag
+    AMI=zeros(L,2);
+    % fprintf('AMI: 00%%')
+    for i=1:L
+        AMI(i,1)=i;
+        X=x(1:end-i);
+        Y=x(i+1:end);
+        AMI(i,2)=average_mutual_information([X,Y]);
+
+    %     fprintf(repmat('\b',1,4));
+    %     msg=sprintf('%3.0d',floor(i/L*100));
+    %     fprintf([msg '%%']);
     end
-end
+
+    tau=[];
+    for i=2:length(AMI)-1
+        if (AMI(i-1,2)>=AMI(i,2))&&(AMI(i,2)<=AMI(i+1,2))
+            tau(end+1,:)=AMI(i,:);
+        end
+    end
+    ind=find(AMI(:,2)<=(0.2*AMI(1,2)),1,'first'); % finds lag at 20% initial AMI
+    if ~isempty(ind)
+        tau(end+1,:)=AMI(ind,:);
+    end
+    if isempty(tau)
+        if L*1.5>length(x/3)
+            tau=9999;
+        else
+            fprintf('max lag needed to be increased for AMI_Thomas\n')
+            [tau,AMI]=AMI_Thomas20150901(x,floor(L*1.5));
+        end
+    end
 
 function AMI = average_mutual_information(data) 
 % function AMI = average_mutual_information(data) 
@@ -124,21 +124,21 @@ function AMI = average_mutual_information(data)
 % column vectors. 
 % 
 % Output is a scalar. 
-n = length(data); 
-X = data(:,1); 
-Y = data(:,2); 
-% Example below is for normal reference rule in 
-% 2 dims, Scott (1992). 
-hx = std(X)/(n^(1/6)); 
-hy = std(Y)/(n^(1/6)); 
-% Compute univariate marginal density functions. 
-P_x = univariate_kernel_density(X, X, hx); 
-P_y = univariate_kernel_density(Y, Y, hy); 
-% Compute joint probability density. 
-JointP_xy = bivariate_kernel_density(data, data, hx, hy); 
-AMI = sum(log2(JointP_xy./(P_x.*P_y)))/n; 
+    n = length(data); 
+    X = data(:,1); 
+    Y = data(:,2); 
+    % Example below is for normal reference rule in 
+    % 2 dims, Scott (1992). 
+    hx = std(X)/(n^(1/6)); 
+    hy = std(Y)/(n^(1/6)); 
+    % Compute univariate marginal density functions. 
+    P_x = univariate_kernel_density(X, hx); 
+    P_y = univariate_kernel_density(Y, hy); 
+    % Compute joint probability density. 
+    JointP_xy = bivariate_kernel_density(data, data, hx, hy); 
+    AMI = sum(log2(JointP_xy./(P_x.*P_y)))/n; 
 
-function y = univariate_kernel_density(value, data, window) 
+function y = univariate_kernel_density(value, window) 
 % function y = univariate_kernel_density(value, 
 % data, window) 
 % Estimates univariate density using kernel 
@@ -150,17 +150,16 @@ function y = univariate_kernel_density(value, data, window)
 %          window (scalar), used for the width of 
 %             density estimation. 
 % Output is an m-vector of probabilities. 
-h = window; 
-n = length(data); 
-m = length(value); 
-% We use matrix operations to speed up computation 
-% of a double-sum. 
-Prob = zeros(n, m); 
-G = Extended(value, n); 
-H = Extended(data', m); 
-Prob = normpdf((G - H)/h); 
-fhat = sum(Prob)/(n*h); 
-y = fhat';
+    %y = univariate_kernel_density_mex(data, window);
+    h = window; 
+    n = length(data); 
+    m = length(value); 
+    % We use matrix operations to speed up computation 
+    % of a double-sum. 
+    Prob = zeros(n, m); 
+    Prob = normpdf((value - value')/h); % Older version had G' as H.
+    fhat = sum(Prob)/(n*h); 
+    y = fhat';
 
 function y = bivariate_kernel_density(value, data, Hone, Htwo) 
 % function y = bivariate_kernel_density(value, 
@@ -175,24 +174,24 @@ function y = bivariate_kernel_density(value, data, Hone, Htwo)
 %           for the widths of density estimation. 
 % Output is an m-vector of probabilities estimated 
 % at the values in ’value’. 
-s = size(data); 
-n = s(1); 
-t = size(value); 
-number_pts = t(1); 
-rho_matrix = corr(data); 
-rho = rho_matrix(1,2); 
-% The adjusted covariance matrix: 
-W = [Hone^2 rho*Hone*Htwo; rho*Hone*Htwo Htwo^2]; 
-Differences = linear_depth(value,-data); 
-prob = mvnpdf(Differences,[0 0],W); 
-Cumprob = cumsum(prob); 
-y(1) = (1/n)*Cumprob(n); 
-for i = 2:number_pts 
-index = n*i; 
-y(i) = (1/(n))* (Cumprob(index)-Cumprob(index - n)); 
-i = i + 1; 
-end 
-y = y'; 
+    s = size(data); 
+    n = s(1); 
+    t = size(value); 
+    number_pts = t(1); 
+    rho_matrix = corr(data); 
+    rho = rho_matrix(1,2); 
+    % The adjusted covariance matrix: 
+    W = [Hone^2 rho*Hone*Htwo; rho*Hone*Htwo Htwo^2]; 
+    Differences = linear_depth(value,-data); 
+    prob = mvnpdf(Differences,[0 0],W); 
+    Cumprob = cumsum(prob); 
+    y(1) = (1/n)*Cumprob(n); 
+    for i = 2:number_pts 
+        index = n*i; 
+        y(i) = (1/(n))* (Cumprob(index)-Cumprob(index - n)); 
+        i = i + 1; 
+    end 
+    y = y'; 
 
 function y = linear_depth(feet, toes) 
 % linear_depth takes a matrix ‘feet’ and lengthens 
@@ -201,39 +200,4 @@ function y = linear_depth(feet, toes)
 % lengthened ‘feet’ and ‘toes’ matrices to achieve 
 % all sum combinations of their rows. 
 % feet and toes have the same number of columns 
-if size(feet, 2) == size(toes, 2) 
-a = size(feet, 1); 
-b = size(toes, 1); 
-Blocks = zeros(a*b, size(toes, 2)); 
-Bricks = Blocks; 
-for i = 1:a 
-Blocks((i-1)*b + 1: i*b,:) = Extended(feet(i,:),b); 
-Bricks((i-1)*b + 1: i*b,:) = toes; 
-i = i + 1; 
-end 
-end 
-y = Blocks + Bricks; 
-
-function y = Extended(vector,n) 
-% Takes an m-dimensional row vector and outputs an 
-% n-by-m matrix with n-many consecutive repeats of 
-% the vector. Similarly,  it takes an 
-% m-dimensional column vector and outputs an 
-% m-by-n matrix. 
-% Else, it returns the original input. 
-M = vector; 
-if size(vector,1) == 1 
-M = zeros(n,length(vector)); 
-for i = 1:n 
-M(i,:) = vector; 
-i = i + 1; 
-end 
-end 
-if size(vector,2) == 1 
-M = zeros(length(vector),n); 
-for i = 1:n 
-M(:,i) = vector; 
-i = i + 1; 
-end 
-end 
-y = M;
+    y = linear_depth_mex(feet);
